857b7ca44a89a94acc84db5503561243dc76973b
SamlaManathe <samlamanathe@gmail.com>
2026-08-18 08:31:08 -0300

    Insere estrutura inicial

---
+++ Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sistema_pagamentos_loja.Entity
+{
+    internal class Cliente
+    {
+    }
+}
---
+++ Pagamentos/PagamentoDinheiro.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sistema_pagamentos_loja.Pagamentos
+{
+    internal class PagamentoDinheiro
+    {
+    }
+}
